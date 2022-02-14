cf319f3c8eef021a5b7624d89baecc219ec9f5b6
aludi <ludivanleeuwen@gmail.com>
2022-02-14 14:01:21 +0100

    attempted push

--- BN.py
+++ BN.py
@@ -209,11 +209,11 @@ def get_temporal_ordering_nodes(experiment, global_state_csv):
         flag = "def"
 
     for key in experiment.reporters.temporal_dict.keys():
-        #print(key, experiment.reporters.temporal_dict[key])
-        #print(len(experiment.reporters.relevant_events) - len(experiment.reporters.evidence_list))
+        print(key, experiment.reporters.temporal_dict[key])
+        print(len(experiment.reporters.relevant_events) - len(experiment.reporters.evidence_list))
         #print(experiment.reporters.evidence_list)
-        #print(experiment.reporters.temporal_list)
-        #print(len(key))
+        print(experiment.reporters.temporal_list)
+        print(len(key))
 
         if len(key) == len(experiment.reporters.relevant_events) - len(experiment.reporters.evidence_list):
             if experiment.reporters.temporal_dict[key] > max_score:
